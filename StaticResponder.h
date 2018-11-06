f11fd326c2fba27bba0d6c72cdfd044739ed5cf8
Ted Percival <ted@tedp.id.au>
2018-11-06 00:21:10 -0700

    Tidy up StaticResponder

--- StaticResponder.h
+++ StaticResponder.h
@@ -7,27 +7,16 @@ namespace shitty {
 
 class StaticResponder: public Handler {
 public:
-#if 1
     // Allow construction using all Response c'tors
     template <typename... Args>
     StaticResponder(Args... args):
-        //response_(std::forward(args)...)
         response_(args...)
     {}
-#else
-    StaticResponder(Response&& response);
-#endif
 
     void handle(Request&& req, Transport *transport) override;
 
 private:
     Response response_;
 };
-
-#if 0
-StaticResponder::StaticResponder(Response&& response):
-    response_(std::move(response))
-{}
-#endif
 
 }

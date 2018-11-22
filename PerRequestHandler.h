1c427988829f5b3eaa9b3e5e266cb238c40c77d6
Ted Percival <ted@tedp.id.au>
2018-11-21 23:08:39 -0700

    Allow PerRequestHandler to be constructed with args

--- PerRequestHandler.h
+++ PerRequestHandler.h
@@ -1,16 +1,26 @@
 #pragma once
 
+#include <tuple>
+
 #include "Handler.h"
 
 namespace shitty {
 
-template <typename HandlerT>
+template <typename HandlerT, typename... HandlerArgs>
 class PerRequestHandler: public Handler {
+public:
+    PerRequestHandler(HandlerArgs... args):
+        init_args_(std::move(args)...)
+    {}
+
     void handle(Request&& request, ServerTransport *transport) override {
-        HandlerT instance;
+        auto instance = std::make_from_tuple<HandlerT>(init_args_);
 
         instance.handle(std::move(request), transport);
     }
+
+private:
+    std::tuple<HandlerArgs...> init_args_;
 };
 
 }

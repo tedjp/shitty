e0079499e8020fc66db2c5f52929d2dc496b3055
Ted Percival <ted@tedp.id.au>
2018-11-25 22:39:49 -0700

    Remove obsolete code

--- shitty.cpp
+++
@@ -1,6 +0,0 @@
-#include "server.h"
-
-int main(void) {
-    shitty::http2::Server().run();
-    return 0;
-}
--- stream.h
+++
@@ -1,8 +0,0 @@
-#include "frame.h"
-
-namespace shitty::http2 {
-class Stream {
-public:
-    void onFrame(Frame& frame);
-};
-} // namespace shitty::http2

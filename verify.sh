d030347123cc41e13d423ce855fbde849181ff67
T Percival <ted.percival@gmail.com>
2023-07-13 01:26:24 -0700

    Run tests against port 8080

--- verify.sh
+++ verify.sh
@@ -5,11 +5,11 @@ set -x
 # These are intended to be run against the `print-requests` server, or
 # equivalent.
 
-curl -v --http1.1 http://localhost/
-curl -v --http2 http://localhost/
-curl -v --http2-prior-knowledge http://localhost/
+curl -v --http1.1 http://localhost:8080/
+curl -v --http2 http://localhost:8080/
+curl -v --http2-prior-knowledge http://localhost:8080/
 
 # Short HTTP1 request
-echo -en "GET / HTTP/1.1\r\nConnection: close\r\n\r\n" | nc -v localhost 80
+echo -en "GET / HTTP/1.1\r\nConnection: close\r\n\r\n" | nc -v localhost 8080
 
 echo OK
